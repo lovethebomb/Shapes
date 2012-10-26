e4deee92e6448fc9e9706a9ed429b431d088cd01
Lucas <lucas@lovethebomb.eu>
2012-10-26 10:05:42 +0200

    Initial Commit

---
+++ Shapes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Shapes
+//
+//  Created by HEYMES Lucas on 26/10/12.
+//  Copyright (c) 2012 Heymès Lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
